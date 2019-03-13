1cb39d2f866219bd4949c3ba7089b8737d7c1591
c1045187 <kevinvansant@gmail.com>
2019-03-13 15:02:08 +0100

    test

--- Project/Startup.cs
+++ Project/Startup.cs
@@ -68,7 +68,8 @@ namespace Project
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                    template: "{controller=Home}/{action=Index}/{id?}");
+        });
         }
     }
 }
