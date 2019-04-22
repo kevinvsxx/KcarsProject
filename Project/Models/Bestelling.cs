1191c06966a30a94c5c551dd8edf67ca8179b430
Kevin Van Sant <kevinvansant@gmail.com>
2019-04-22 20:15:04 +0200

    verder gewerkt

---
+++ Project/Models/Bestelling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Bestelling
+    {
+        public int BestellingID { get; set; }
+        public String Artikel { get; set; }
+        public decimal prijs { get; set; }
+        public int klantID { get; set; }
+        public Klant klant { get; set; }
+    }
+}
---
+++ Project/Models/Klant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Klant
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        [Display(Name = "Datum aangemaakt")]
+        [DataType(DataType.Date)]
+        public DateTime AangemaaktDatum { get; set; }
+        public ICollection<Bestelling> Bestellingen { get; set; }
+        public string klantDisplay
+        {
+            get
+            {
+                return $"{Voornaam} {Naam}";
+            }
+        }
+    }
+}
