334ad131a2a009273899ac04421a55a255de9117
KevinVanSant <kevinvansant@gmail.com>
2019-12-02 21:42:19 +0100

    database geupdate

--- Project/Migrations/20191202195548_LocalTest.Designer.cs
+++ Project/Migrations/20191202195548_LocalTest.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.Data;
 
 namespace Project.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20191202195548_LocalTest")]
+    partial class LocalTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
